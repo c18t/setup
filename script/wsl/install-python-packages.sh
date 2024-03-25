957be0a4a59469af22d9e216a0474f03e70348a3
ɯ̹t͡ɕʲi <xc18tx@gmail.com>
2024-03-26 00:28:55 +0900

    resolve externally-managed-environment error during pip3 installation

--- script/wsl/install-python-packages.sh
+++ script/wsl/install-python-packages.sh
@@ -35,7 +35,7 @@ PYWINRM=$?
 # install python pacakges for ansible
 if [ $PYWINRM -ne 0 ]; then
     echo install python pacakges for ansible ...
-    pip3 install pywinrm
+    pip3 install --break-system-package pywinrm 
     result=$?
     if [ $result -eq 0 ]; then
         echo ... done!
