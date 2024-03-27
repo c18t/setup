a4c81e52f21ee6e3fc60d347124c2aa728866eda
ɯ̹t͡ɕʲi <xc18tx@gmail.com>
2024-03-28 00:11:12 +0900

    remove anyenv and and update anyenv role to uninstall its resources

--- ansible/playbooks/files/fisher-my-setup/functions/update_anyenvrc.fish
+++
@@ -1,9 +0,0 @@
-function update_anyenvrc --description 'Update conf.d/anyenv.fish'
-    set -l anyenvrc $__fish_config_dir/conf.d/anyenv.fish
-    echo 'if status --is-login' >$anyenvrc
-    echo '    and type anyenv >/dev/null 2>&1' >>$anyenvrc
-    echo >>$anyenvrc
-    anyenv init - --no-rehash >>$anyenvrc
-    echo >>$anyenvrc
-    echo end >>$anyenvrc
-end
